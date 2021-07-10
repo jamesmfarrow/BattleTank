9dec1306e12899f42628f05b8a56cd8f9e8e8c40
James Farrow <jamesmfarrow@yahoo.co.uk>
2021-07-10 16:04:32 +0100

    Lecture 215 fixed ensure bug AIcontroller

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -19,7 +19,7 @@ void ATankAIController::Tick(float DeltaTime)
     auto PlayerTank{GetWorld()->GetFirstPlayerController()->GetPawn()};
     auto ControlledTank{GetPawn()};
     
-    if(!ensure(PlayerTank && ControlledTank)) return;
+    if(!(PlayerTank && ControlledTank)) return;
     // move towards player
     MoveToActor(PlayerTank, AcceptanceRadius); // TO DO check units of AcceptanceRadius
 
